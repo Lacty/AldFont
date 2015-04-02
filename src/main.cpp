4effe201f8a48ee9d9a30375fac2d81b151a7336
U-PS14B22\vantan <akira206@gmail.com>
2015-04-03 07:50:00 +0900

    ko-do

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+﻿
+#include "common.h"
+#include "ald_font.h"
+#include "float2.h"
+#include <string>
+
+
+int main() {
+  AppEnv env(WIDTH, HEIGHT);
+
+  cAldFont ald_test("res/font/chogokubosogothic_5.ttf");
+  ald_test.size(60);
+
+  std::string str = "ALD_FONT_TEST";
+  float2 pos = float2(-240, 0);
+  Color color = Color::green;
+  float time = 0.15f;
+
+  env.bgColor(Color(1, 1, 1));
+
+  while (env.isOpen()) {
+    env.begin();
+
+    ald_test.draw(str, pos, time, color);
+
+    env.end();
+  }
+}
